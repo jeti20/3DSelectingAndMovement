9eb6d29c0f0836d5b568cbea59c7e3bed41aeae1
Dehydrogenase <michalrodak98@gmail.com>
2024-01-09 12:14:24 +0100

    Added impact of endurance on tracking units

--- 3DWithIsometricView/Assets/RandomStats.cs
+++ 3DWithIsometricView/Assets/RandomStats.cs
@@ -7,12 +7,18 @@ public class RandomStats : MonoBehaviour
     public float agility;
     public float endurance;
     public float stopDistance = 2.0f;
+    public float originalSpeed;
+    public float originalEndurance;
+    public float regenerationSpeed = 1.0f;
+    private bool canMove = false; 
 
     private void Start()
     {
         speed = Random.Range(1f, 3f);
         agility = Random.Range(1f, 3f);
-        endurance = Random.Range(1f, 3f);
+        endurance = Random.Range(1f, 2f);
+        originalSpeed = speed;
+        originalEndurance = endurance;
     }
 
     private void Update()
@@ -25,19 +31,39 @@ public class RandomStats : MonoBehaviour
             // Adjust the speed based on the speed variable
             float currentSpeed = speed * Time.deltaTime;
 
-            if (distance > stopDistance)
+            if (endurance > 0 && canMove)
             {
-                // Set up the target position from the target
-                Vector3 targetPosition = selectedUnit.transform.position + (selectedUnit.transform.forward * stopDistance);
-
-                // Move to the target unit (unitSelected)
-                transform.position = Vector3.MoveTowards(transform.position, targetPosition, currentSpeed);
-                //added this because before when player changed the selected unit, the unit that become not selected was not following the new selected obj, some blue arrow showed oposite direction, some kind of force
-                if (this != selectedUnit)
+                speed = originalSpeed;
+                if (distance > stopDistance)
                 {
-                    NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
-                    navMeshAgent.ResetPath();
+                    // Set up the target position from the target
+                    Vector3 targetPosition = selectedUnit.transform.position + (selectedUnit.transform.forward * stopDistance);
+
+                    // Move to the target unit (unitSelected)
+                    transform.position = Vector3.MoveTowards(transform.position, targetPosition, currentSpeed);
+                    endurance = Mathf.Clamp(endurance - Time.deltaTime, 0, originalEndurance); // reducing endurane [0, originalEndurance]
+                    // Added this because before when player changed the selected unit, the unit that became not selected was not following the new selected obj, some blue arrow showed the opposite direction, some kind of force
+                    if (this != selectedUnit)
+                    {
+                        NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+                        navMeshAgent.ResetPath();
+                    }
+                }              
+            }
+            else
+            {
+                // if unit has max endurance can move
+                speed = 0;
+                if (endurance == originalEndurance)
+                {
+                    canMove = true;
                 }
+                else
+                {
+                    canMove = false;
+                }
+                
+                endurance = Mathf.Clamp(endurance + Time.deltaTime * regenerationSpeed, 0, originalEndurance);
             }
         }
     }
